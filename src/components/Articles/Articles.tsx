92fa84865785a1a25749292f515d79a517f3da7e
powerty <qwercy142@gmail.com>
2021-06-08 00:51:04 +0200

    Create Articles

---
+++ src/components/Articles/Articles.tsx
@@ -0,0 +1,32 @@
+import * as S from "./Articles.style";
+
+const Articles = () => {
+  return (
+    <S.Wrapper>
+      <article>
+        <h3> Play Free Sudoku Now! </h3>
+        <p>
+          Sudoku is one of the most popular puzzle games of all time. The goal
+          of Sudoku is to fill a 9×9 grid with numbers so that each row, column
+          and 3×3 section contain all of the digits between 1 and 9. As a logic
+          puzzle, Sudoku is also an excellent brain game. If you play Sudoku
+          daily, you will soon start to see improvements in your concentration
+          and overall brain power. Start a game now. Within no time Sudoku free
+          puzzles will be your favorite online game.
+        </p>
+      </article>
+
+      <article>
+        <h3> About Sudoku</h3>
+        <p>
+          The popular Japanese puzzle game Sudoku is based on the logical
+          placement of numbers. An online game of logic, Sudoku doesn’t require
+          any calculation nor special math skills; all that is needed are brains
+          and concentration.
+        </p>
+      </article>
+    </S.Wrapper>
+  );
+};
+
+export default Articles;
