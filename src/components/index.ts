52273f6226edf1cdec06ba17cfac33465905c672
powerty <qwercy142@gmail.com>
2021-06-08 00:50:56 +0200

    Default export components

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Cell } from "./Cell/Cell";
+export { default as Stats } from "./Stats/Stats";
+export { default as Articles } from "./Articles/Articles";
+export { default as Controlers } from "./Controlers/Controlers";
